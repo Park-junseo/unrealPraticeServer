b5f1a4b88bdc1dcacb3474c9c825ecdc0fa467d6
박준서 <psbsanno.com@gmail.com>
2022-06-27 16:25:39 +0900

    error

--- ServerCore/CorePch.h
+++ ServerCore/CorePch.h
@@ -1,4 +1,16 @@
 #pragma once
-class CorePch
-{
-};
+
+#include "Types.h"
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <iostream>
+using namespace std;
+
+void HelloWorld();
