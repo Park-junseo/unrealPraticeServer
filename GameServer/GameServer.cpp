5f2b3f41ac8b3f6785b72bdf77e320444c38ac4d
박준서 <psbsanno.com@gmail.com>
2022-06-28 09:29:43 +0900

    eax

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,18 +2,71 @@
 #include <iostream>
 #include "CorePch.h"
 
-int main()
+#include <thread>
+
+void HelloThread()
 {
-    HelloWorld();
+	cout << "Hello Thread" << endl;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+void HelloThread_2(int32 num)
+{
+	cout << num << endl;
+}
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+int32 sum = 0;
+
+void Add()
+{
+	for (int32 i = 0; i < 100'0000; i++)
+	{
+		int32 eax = sum;
+		eax = eax + 1;
+		sum = eax;
+	}
+}
+
+void Sub()
+{
+	for (int32 i = 0; i < 100'0000; i++)
+	{
+		int32 eax = sum;
+		eax = eax - 1;
+		sum = eax;
+	}
+}
+
+int main()
+{
+	Add();
+	Sub();
+
+	cout << sum << endl;
+
+	std::thread t1(Add);
+	std::thread t2(Sub);
+	t1.join();
+	t2.join();
+	cout << sum << endl;
+
+	//vector<std::thread> v;
+
+	//for (int32 i = 0; i < 10; i++)
+	//{
+	//	v.push_back(std::thread(HelloThread_2, i));
+	//}
+
+	////std::thread t(HelloThread_2, 10);
+	////
+	////if(t.joinable())
+	////	t.join();
+
+	//for (int32 i = 0; i < 10; i++)
+	//{
+	//	if (v[i].joinable())
+	//		v[i].join();
+	//}
+
+	//cout << "Hello Main" << endl;
+
+}
