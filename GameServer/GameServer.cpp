639dbd7621fe6db8fd6052e7ebfafa9e6f31bce1
박준서 <psbsanno.com@gmail.com>
2022-06-28 14:02:25 +0900

    spinLock

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -9,6 +9,90 @@
 #include "AccountManager.h"
 #include "UserManager.h"
 
+class SpinLock
+{
+public:
+	void lock()
+	{
+		//CAS (Compare-And-Swap)
+		
+		bool expected = false;
+		bool desired = true;
+
+		//// CAS 의사 코드
+		//if (_locked == expected)
+		//{
+		//	expected = _locked;
+		//	_locked = desired;
+		//	return true;
+		//}
+		//else
+		//{
+		//	expected = _locked;
+		//	return false;
+		//}
+		
+		//epxrected: locked 값을 예상해주고, desired: locked값이 바뀌길 원하는 값
+		while (_locked.compare_exchange_strong(expected, desired) == false) //expected에 _locked 값이 들어가고 expected가 desired 값이 되면 성공 true 반환
+		{
+			expected = false;
+		}
+
+		//while (_locked)
+		//{
+
+		//}
+		//_locked = true;
+	}
+
+	void unlock()
+	{
+		//_locked = false;
+		_locked.store(false); //_locked 변수가 atomic임을 알림
+	}
+
+private:
+	//volatile: 컴파일러에게 최적화 X
+	//volatile bool _locked = false;
+
+	atomic<bool> _locked = false;
+};
+
+int32 sum = 0;
+mutex m;
+SpinLock spinLock;
+
+void Add()
+{
+	for (int32 i = 0; i < 10'0000; i++)
+	{
+		lock_guard<SpinLock> guard(spinLock);
+		sum++;
+	}
+}
+
+void Sub()
+{
+	for (int32 i = 0; i < 10'0000; i++)
+	{
+		lock_guard<SpinLock> guard(spinLock);
+		sum--;
+	}
+}
+
+int main()
+{
+	thread t1(Add);
+	thread t2(Sub);
+
+	t1.join();
+	t2.join();
+
+	cout << sum << endl;
+}
+
+/*
+
 void Func1()
 {
 	for (int32 i = 0; i < 100; i++)
@@ -44,6 +128,9 @@ int main()
 	//lock_guard<mutex> g1(m1, std::adopt_lock);
 	//lock_guard<mutex> g1(m2, std::adopt_lock);
 }
+
+*/
+/*
 
 vector<int32> v; //atomic 사용불가
 
@@ -93,18 +180,21 @@ void Push()
 	}
 }
 
-//int main()
-//{
-//	//v.reserve(20000);
-//
-//	std::thread t1(Push);
-//	std::thread t2(Push);
-//
-//	t1.join();
-//	t2.join();
-//
-//	cout << v.size() << endl;
-//}
+int main()
+{
+	//v.reserve(20000);
+
+	std::thread t1(Push);
+	std::thread t2(Push);
+
+	t1.join();
+	t2.join();
+
+	cout << v.size() << endl;
+}
+*/
+
+/*
 
 // atomic : All-Or-Nothing
 
@@ -135,19 +225,23 @@ void Sub()
 	}
 }
 
-//int main()
-//{
-//	Add();
-//	Sub();
-//
-//	cout << sum << endl;
-//
-//	std::thread t1(Add);
-//	std::thread t2(Sub);
-//	t1.join();
-//	t2.join();
-//	cout << sum << endl;
-//}
+int main()
+{
+	Add();
+	Sub();
+
+	cout << sum << endl;
+
+	std::thread t1(Add);
+	std::thread t2(Sub);
+	t1.join();
+	t2.join();
+	cout << sum << endl;
+}
+
+*/
+
+/*
 
 void HelloThread()
 {
@@ -159,25 +253,27 @@ void HelloThread_2(int32 num)
 	cout << num << endl;
 }
 
-//int main()
-//{
-//	vector<std::thread> v;
-//
-//	for (int32 i = 0; i < 10; i++)
-//	{
-//		v.push_back(std::thread(HelloThread_2, i));
-//	}
-//
-//	//std::thread t(HelloThread_2, 10);
-//	//
-//	//if(t.joinable())
-//	//	t.join();
-//
-//	for (int32 i = 0; i < 10; i++)
-//	{
-//		if (v[i].joinable())
-//			v[i].join();
-//	}
-//
-//	cout << "Hello Main" << endl;
-//}
+int main()
+{
+	vector<std::thread> v;
+
+	for (int32 i = 0; i < 10; i++)
+	{
+		v.push_back(std::thread(HelloThread_2, i));
+	}
+
+	//std::thread t(HelloThread_2, 10);
+	//
+	//if(t.joinable())
+	//	t.join();
+
+	for (int32 i = 0; i < 10; i++)
+	{
+		if (v[i].joinable())
+			v[i].join();
+	}
+
+	cout << "Hello Main" << endl;
+}
+
+*/
