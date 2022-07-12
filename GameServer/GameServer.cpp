9b643653bcb084d99bedf5eb4827037c6551fc4e
박준서 <psbsanno.com@gmail.com>
2022-07-12 10:01:47 +0900

    countPrime

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -12,10 +12,77 @@
 
 #include "ThreadManager.h"
 
+// 소수 구하기
+#include <vector>
+
+bool IsPrime(int number)
+{
+	if (number <= 1)
+		return false;
+	if (number == 2 || number == 3)
+		return true;
+
+	for (int i = 2; i < number; i++)
+	{
+		if ((number % i) == 0)
+			return false;
+	}
+
+	return true;
+}
+
+// [start - end]
+int CountPrime(int start, int end)
+{
+	int count = 0;
+	
+	for (int number = start; number <= end; number++)
+	{
+		if (IsPrime(number))
+			count++;
+	}
+
+	return count;
+}
+
+// 1과 자기 자신으로만 나뉘면 그것을 소수라고 함
+
+int main()
+{
+	const int MAX_NUMBER = 100'0000;
+	// 1-MAX_NUMBER까지의 소수 개수
+
+	vector<thread> threads;
+
+	int coreCount = thread::hardware_concurrency();
+
+	cout << "CoreCount: " << coreCount << endl;
+
+	int jobCount = (MAX_NUMBER / coreCount) + 1;
+
+	atomic<int> primeCount = 0;
+
+	for (int i = 0; i < coreCount; i++)
+	{
+		int start = (i * jobCount) + 1;
+		int end = min(MAX_NUMBER, ((i + 1) * jobCount));
+
+		threads.push_back(thread([start, end, &primeCount]()
+			{
+				primeCount += CountPrime(start, end);
+			}));
+	}
+
+	for (thread& t : threads)
+		t.join();
+
+	cout << primeCount << endl;
+}
+
+// DeadLock Profiler
+/*
 #include "PlayerManager.h"
 #include "LAccountManager.h"
-
-
 
 int main()
 {
@@ -42,7 +109,7 @@ int main()
 
 	GThreadManager->Join();
 }
-
+*/
 //RW Lock
 /*
 class TestLock
