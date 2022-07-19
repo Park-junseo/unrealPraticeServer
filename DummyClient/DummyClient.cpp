c303a425ca30e461edc9bc1bc02d110f0a24324f
박준서 <psbsanno.com@gmail.com>
2022-07-19 11:09:02 +0900

    UDP

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -6,6 +6,101 @@
 #include <WS2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
+void HandleError(const char* cause)
+{
+	int32 errCode = ::WSAGetLastError();
+	cout << cause << " ErrorCode : " << errCode << endl;
+}
+
+int main()
+{
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError(); // 어떤 이유로 안되는지 에러 코드를 받아옴
+		cout << "Soket ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// 연결할 목적지: IP주소 + Port
+	SOCKADDR_IN serverAddr; // IPv4 구조체
+	::memset(&serverAddr, 0, sizeof(serverAddr)); // 구조체 초기화
+	serverAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);
+
+	// connected UDP
+	// 소켓 자체에 즐겨찾기 처럼 대상을 정해줌
+	// sendTo, recvFrom 으로 serverAddr 인자를 넘기지 않아도 됨
+	::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+	
+
+	while (true)
+	{
+		// TODO
+
+		for (int32 i = 0; i < 10; i++)
+		{
+			char sendBuffer[100] = "Hello World!";
+
+			// Unconnected UDP
+			//int32 resultCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0,
+			//	(SOCKADDR*)&serverAddr, sizeof(serverAddr));
+
+			// Connected UDP
+			int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+
+			if (resultCode == SOCKET_ERROR)
+			{
+				HandleError("SendTo)");
+				return 0;
+			}
+
+			cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
+		}
+
+
+		SOCKADDR_IN recvAddr;
+		::memset(&recvAddr, 0, sizeof(recvAddr));
+		int32 addrLen = sizeof(recvAddr);
+
+		char recvBuffer[1000];
+
+		// 패킷을 보낸 client의 주소와 버퍼를 받아옴
+		// Unconnected UDP
+		//int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0,
+		//	(SOCKADDR*)&recvAddr, &addrLen);
+
+		// Connected UDP
+		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+
+		if (recvLen <= 0)
+		{
+			HandleError("RecvFrom");
+			return 0;
+		}
+
+		cout << "Recv Data! Data = " << recvBuffer << endl;
+		cout << "Recv Data! Len = " << recvLen << endl;
+
+		this_thread::sleep_for(1s);
+	} 
+
+	// ------------------------
+
+	// 소켓 리소스 반환
+	::closesocket(clientSocket);
+
+	// 원속 종료
+	::WSACleanup();
+}
+
+//TCP
+/*
 int main()
 {
 	// 원속 초기화 (ws2_32 라이브러리 초기화)
@@ -98,3 +193,4 @@ int main()
 	// 원속 종료
 	::WSACleanup();
 }
+*/
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -17,6 +17,76 @@
 #include <WS2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
+void HandleError(const char* cause)
+{
+	int32 errCode = ::WSAGetLastError();
+	cout << cause << " ErrorCode : " << errCode << endl;
+}
+
+int main()
+{
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	if (serverSocket == INVALID_SOCKET)
+	{
+		HandleError("Socket");
+		return 0;
+	}
+
+	SOCKADDR_IN serverAddr; // IPv4 구조체
+	::memset(&serverAddr, 0, sizeof(serverAddr)); // 구조체 초기화
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); // 자신의 아이피로 알아서 지정
+	serverAddr.sin_port = ::htons(7777); //포트의 임의 번호 지정
+
+	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		HandleError("Bind");
+		return 0;
+	}
+
+	while (true)
+	{
+		SOCKADDR_IN clientAddr;
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+
+		char recvBuffer[1000];
+
+		//패킷을 보낸 client의 주소와 버퍼를 받아옴
+		int32 recvLen = ::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0,
+			(SOCKADDR*)&clientAddr, &addrLen);
+
+		if (recvLen <= 0)
+		{
+			HandleError("RecvFrom");
+			return 0;
+		}
+
+		cout << "Recv Data! Data = " << recvBuffer << endl;
+		cout << "Recv Data! Len = " << recvLen << endl;
+
+		int32 errorCode = ::sendto(serverSocket, recvBuffer, recvLen, 0, 
+			(SOCKADDR*)&clientAddr, sizeof(clientAddr));
+
+		if (errorCode == SOCKET_ERROR)
+		{
+			HandleError("SendTo");
+			return 0;
+		}
+
+		cout << "Send Data! Len = " << recvLen << endl;
+	}
+
+	// 원속 종료
+	::WSACleanup();
+}
+
+// TCP
+/*
 int main()
 {
 	// 원속 초기화 (ws2_32 라이브러리 초기화)
@@ -120,7 +190,7 @@ int main()
 	// 원속 종료
 	::WSACleanup();
 }
-
+*/
 //TypeCast
 /*
 #include "RefCounting.h"
