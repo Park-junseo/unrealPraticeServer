43c24185161f2d21e4184cffc2c16864a6ba3430
박준서 <psbsanno.com@gmail.com>
2022-07-20 16:38:01 +0900

    Select Model

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -17,13 +17,166 @@
 #include <WS2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
-
 void HandleError(const char* cause)
 {
 	int32 errCode = ::WSAGetLastError();
 	cout << cause << " ErrorCode : " << errCode << endl;
 }
 
+const int32 BUFSIZE = 1000;
+// 클라이언트가 접속할 떄 Session 구조체로 정보를 관리
+struct Session
+{
+	SOCKET socket = INVALID_SOCKET;
+	char recvBuffer[BUFSIZE];
+	int32 recvBytes = 0;
+	int32 sendBytes = 0;
+};
+
+int main()
+{
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	// 블로킹(Blocking) 소켓
+	// accept -> 접속한 클라가 있을 때
+	// connect -> 서버 접속 성공했을 때
+	// send, sendto -> 요청한 데이터를 송신 버퍼에 복사했을 때
+	// recv, recvfrom -> 수신 버퍼에 도착한 데이터가 있고, 이를 유저레벨 버퍼에 복사했을 떄 성공
+
+	// 논블로킹(Non-Blocking)
+
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		HandleError("Socket");
+		return 0;
+	}
+
+	u_long on = 1;
+	// 논블로킹 소켓으로 변환
+	if (::ioctlsocket(listenSocket, FIONBIO, &on) == INVALID_SOCKET)
+		return 0;
+
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	serverAddr.sin_port = ::htons(7777);
+
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		return 0;
+
+	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+		return 0;
+
+	cout << "Accept" << endl;
+
+	// Select 모델 = (select 함수가 핵심이 되는)
+	// 소켓 함수 호출이 성공할 시점을 미리 알 수 있다!
+	// 클라이언트같은 적은 버퍼 환경에서 유용
+	// 문제 상황)
+	// 수신버퍼에 데이터가 없는데, read 한다거나
+	// 송신버퍼가 꽉 찼는데, write 한다거나
+	// - 블로킹 소켓 : 조건이 만족되지 않아서 블로킹되는 상황 예방
+	// - 논블로킹 소켓 : 조건이 만족되지 않아서 불필요하게 반복 쳌하는 상황을 예방
+
+	// socket set // 읽기 또는 쓰기 소켓에 대한 원하는 시점을 관찰
+	// 1) 읽기[ ] 쓰기[ ] 예외(00B)[ ] 관찰 대상 등록
+	// OutOfBand는 send() 마지막 인자 MSG_OOB로 보내는 특별한 데이터
+	// 받는 쪽에서도 recv 00B 세팅을 해야 읽을 수 있음 <-- 사실상 예외 소켓 셋은 잘 사용하지 않음
+	// 2) select(readSet, writeSet, exceptSet) -> 세 socket set 인자를 받고 관찰 시작
+	// 3) 적어도 하나의 소켓이 준비되면 리턴 -> 낙오자는 알아서 제거됨
+	// 4) 남은 소켓 체크해서 진행 -> 낙오된 소켓은 소켓 셋에 계속 채워줘서 select
+
+	// fd_set set;
+	// FD_ZERO(&set) : 소켓 셋 비우기
+	// FD_SET(s, &set) : 소켓 셋에 소켓 s 넣기
+	// FD_CLR(s, &set) : 소켓 셋에 소켓 s 제거
+	// FD_ISSET(s, &set) : 소켓 s가 set에 들어 있으면 0이 아닌 값 리턴
+
+	vector<Session> sessions; // Session 관리하는 벡터
+	sessions.reserve(100);
+
+	fd_set reads;
+	fd_set writes;
+
+	while (true)
+	{
+		// 소켓 셋 초기화
+		FD_ZERO(&reads);
+		FD_ZERO(&writes);
+
+		// ListenSocket 등록
+		FD_SET(listenSocket, &reads); // 처음 accept 하기 위해
+
+		// 알맞게 소켓 등록
+		for (Session& s : sessions)
+		{
+			if (s.recvBytes <= s.sendBytes)
+				FD_SET(s.socket, &reads); // 읽고 보내야 하므로
+			else
+				FD_SET(s.socket, &writes);
+		}
+
+		// [옵션] 마지막 timeout 인자 설정 가능
+		// 마지막 인자인 timeval을 넣지 않으면 하나의 set 이라도 준비될 때까지 대기
+		timeval timeout;
+		//timeout.tv_sec;
+		//timeout.tv_usec;
+		int32 retVal = ::select(0, &reads, &writes, nullptr, nullptr);
+		if (retVal == SOCKET_ERROR)
+			break;
+		// select 함수에서 하나라도 준비되면 리턴하며 reads와 writes에서 낙오자는 알아서 제거
+
+
+		// Listener 소켓 체크
+		if (FD_ISSET(listenSocket, &reads))
+		{
+			SOCKADDR_IN clientAddr;
+			int32 addrLen = sizeof(clientAddr);
+			SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+			if (clientSocket != INVALID_SOCKET) //select함수로 실행 가능한 소켓이 있으므로
+			{
+				cout << "Client Connected" << endl;
+				sessions.push_back(Session{ clientSocket });
+			}
+		}
+
+		// listenSocket 이외에 나머지 소켓 체크
+		for (Session& s : sessions)
+		{
+			// Read 체크
+			if (FD_ISSET(s.socket, &reads))
+			{
+				int32 recvLen = ::recv(s.socket, s.recvBuffer, BUFSIZE, 0);
+				if (recvLen <= 0)
+				{
+					// TODO : sessions 제거
+					continue;
+				}
+
+				s.recvBytes = recvLen;
+			}
+
+			// Write 체크
+			if(FD_ISSET(s.socket, &writes))
+			{
+				// 블로킹 모드였을 경우 -> 모든 데이터 다 보냄
+				// 논블로킹 모드 -> 일부만 보낼 수 있음 (상대방 수신 버퍼 상황에 따라) 
+				// 
+				::send(s.socket, &s.recvBuffer[s.sendBytes], s.recvBytes - s.sendBytes, 0);
+			}
+		}
+	}
+
+	// 원속 종료
+	::WSACleanup();
+}
+
+// non blocking socket
+/*
 int main()
 {
 	WSAData wsaData;
@@ -127,7 +280,7 @@ int main()
 	// 원속 종료
 	::WSACleanup();
 }
-
+*/
 // socket option
 /*
 int main()
