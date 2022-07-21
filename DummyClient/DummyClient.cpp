e58bb4028f8d5eed59e3b364677f0e6935929dee
박준서 <psbsanno.com@gmail.com>
2022-07-21 18:26:52 +0900

    Overlapped Model ing

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -72,7 +72,7 @@ int main()
 		}
 
 		cout << "Send Data ! Len = " << sizeof(sendBuffer) << endl;
-		
+
 		// Recv
 		while (true)
 		{
@@ -107,6 +107,103 @@ int main()
 	::WSACleanup();
 }
 
+// WSAEventSelect Model
+/*
+int main()
+{
+	this_thread::sleep_for(1s);
+
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		HandleError("Socket");
+		return 0;
+	}
+
+	u_long on = 1;
+	// 논블로킹 소켓으로 변환
+	if (::ioctlsocket(clientSocket, FIONBIO, &on) == INVALID_SOCKET)
+		return 0;
+
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);
+
+	// Connect
+	while (true)
+	{
+		if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		{
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+			// 이미 연결된 상태
+			if (::WSAGetLastError() == WSAEISCONN)
+				break;
+
+			//ERORR
+			break;
+		}
+	}
+
+	cout << "Connected to Server!" << endl;
+
+	char sendBuffer[100] = "Hello World";
+
+	// Send
+	while (true)
+	{
+		if (::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0) == SOCKET_ERROR)
+		{
+			// 논블록킹이 일어날만한 상황
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			//Error
+			break;
+		}
+
+		cout << "Send Data ! Len = " << sizeof(sendBuffer) << endl;
+		
+		// Recv
+		while (true)
+		{
+			char recvBuffer[1000];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen == SOCKET_ERROR)
+			{
+				// 논블록킹이 일어날만한 상황
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
+
+				//Error
+				break;
+			}
+			else if (recvLen == 0)
+			{
+				// 연결 끊김
+				break;
+			}
+
+			cout << "Recv Data Len = " << recvLen << endl;
+			break;
+		}
+
+		this_thread::sleep_for(1s);
+	}
+
+	// 소켓 리소스 반환
+	::closesocket(clientSocket);
+
+	// 원속 종료
+	::WSACleanup();
+}
+*/
 // socket option
 /*
 int main()
