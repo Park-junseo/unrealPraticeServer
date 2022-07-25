d88f1c9c67be65588301ecb3decc25b783ab791c
박준서 <psbsanno.com@gmail.com>
2022-07-25 18:58:09 +0900

    completion 모델

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -17,6 +17,8 @@
 #include <WS2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
+#include "Memory.h"
+
 void HandleError(const char* cause)
 {
 	int32 errCode = ::WSAGetLastError();
@@ -27,10 +29,175 @@ const int32 BUFSIZE = 1000;
 // 클라이언트가 접속할 떄 Session 구조체로 정보를 관리
 struct Session
 {
+	SOCKET socket = INVALID_SOCKET;
+	char recvBuffer[BUFSIZE];
+	int32 recvBytes = 0;
+	int32 sendBytes = 0;
+};
+
+enum IO_TYPE
+{
+	READ,
+	WRITE,
+	ACCEPT,
+	CONNECT
+};
+
+struct OverlappedEx
+{
+	WSAOVERLAPPED overlapped = {};
+	int32 type = 0; // read, write, accpet, connect ..
+};
+
+void CALLBACK RecvCallback(DWORD error, DWORD recvLen, LPWSAOVERLAPPED overlapped, DWORD flags)
+{
+	cout << "Data Recv Len Cal'lback = " << recvLen << endl;
+	// TODO : 에코 서버를 만든다면 WSASend()
+
+	Session* session = (Session*)overlapped;
+}
+
+void WorkerThreadMain(HANDLE iocpHandle)
+{
+	while (true)
+	{
+		//소켓에 송수신된 바이트 수를 넘겨주기 위함
+		DWORD bytesTransferred = 0;
+		Session* session = nullptr;
+		OverlappedEx* overlappedEx = nullptr;
+		BOOL ret = ::GetQueuedCompletionStatus(iocpHandle, &bytesTransferred,
+			(ULONG_PTR*)&session, (LPOVERLAPPED*)&overlappedEx, INFINITE);
+
+		if (ret == FALSE || bytesTransferred == 0)
+		{
+			// TODO : 연결 끊김
+			continue;
+		}
+
+		ASSERT_CRASH(overlappedEx->type == IO_TYPE::READ);
+
+		cout << "Recv Data IOCP = " << bytesTransferred << endl;
+
+		WSABUF wsaBuf;
+		wsaBuf.buf = session->recvBuffer;
+		wsaBuf.len = BUFSIZE;
+
+		// 클라이언트와 1대1 대응이 아니므로 스택메모리가 아닌 동적할당으로 넘겨줄 것
+		//OverlappedEx* overlappedEx = new OverlappedEx();
+		//overlappedEx->type = IO_TYPE::READ;
+
+		DWORD recvLen = 0;
+		DWORD flags = 0;
+		::WSARecv(session->socket, &wsaBuf, 1, &recvLen, &flags, &overlappedEx->overlapped, NULL);
+	}
+}
+
+int main()
+{
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+		return 0;
+
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	serverAddr.sin_port = ::htons(7777);
+
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		return 0;
+
+	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+		return 0;
+
+	cout << "Accept" << endl;
+
+	// Overlapped 모델 (Completion Routine 콜백 기반)
+	// - 비동기 입출력 함수 완료되면, 쓰레드마다 있는 APC 큐에 일감이 쌓임
+	// - Alertable Wait 상태로 들어가서 APC 큐 비우기 (콜백 함수)
+	// 단점) APC 큐 쓰레드마다 있다! Alertable Wait 자체도 조금 부담!
+	// 단점) 이벤트 방식 소켓: 이벤트 1:1 대응
+
+	// IOCP (Completion Port) 모델
+	// - APC -> Completion Port (쓰레드마다 있는 건 아니고 1개. 중앙에서 관리하는 APC 큐?)
+	// - Alertable Wait -> CP 결과 처리를 GetQueuedCompletionStatus
+	// 멀티 쓰레드 환경에서 사용하는 것이 일반적, 쓰레드와 궁합이 굉장히 좋음
+
+	// CreateIoCompletionPort
+	// GetQueuedCompletionStatus
+
+	// 모든 섹션을 들고있는 상태
+	vector<Session*> sessionManager;	
+
+	// CP 생성 또는 기존의 소켓을 IOCP에 등록할 떄
+	// CP 생성일 때는 첫 인자에 INVALID_HANDLE_VALUE
+	HANDLE iocpHandle = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+
+	// WorkerThreads
+	for (int32 i = 0; i < 5; i++)
+		GThreadManager->Launch([=]() {WorkerThreadMain(iocpHandle); });
+
+	// 메인 쓰레드: accpet 담당
+	while (true)
+	{
+		SOCKADDR_IN clientAddr;
+		int32 addrLen = sizeof(clientAddr);
+
+		SOCKET clientSocket = accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+			return 0;
+
+		//Session* session = new Session();
+		Session* session = xnew<Session>();
+		session->socket = clientSocket;
+		sessionManager.push_back(session);
+
+		cout << "Client Connected !" << endl;
+
+		// 소켓을 CP에 등록 (관찰대상임을 알림)
+		::CreateIoCompletionPort((HANDLE)clientSocket, iocpHandle, 
+			(ULONG_PTR)session, // Key, 넘겨준 소켓에 대한 임의의 키값을 설정 가능
+			0 //최대쓰레드, 0 일시 최대 코어 개수만큼 할당
+		);
+
+		WSABUF wsaBuf;
+		wsaBuf.buf = session->recvBuffer;
+		wsaBuf.len = BUFSIZE;
+
+		// 클라이언트와 1대1 대응이 아니므로 스택메모리가 아닌 동적할당으로 넘겨줄 것
+		OverlappedEx* overlappedEx = new OverlappedEx();
+		overlappedEx->type = IO_TYPE::READ;
+
+		DWORD recvLen = 0;
+		DWORD flags = 0;
+		::WSARecv(clientSocket, &wsaBuf, 1, &recvLen, &flags, &overlappedEx->overlapped, NULL);
+
+		// 유저가 게임 접속 종료
+		//Session* s = sessionManager.back();
+		//sessionManager.pop_back();
+		//// 하지만 임의로 삭제 시, 오염된 메모리에 접근할 수 있음. xnew 사용
+		////delete s;
+		//xdelete(s);
+	}
+
+	GThreadManager->Join();
+
+	// 원속 종료
+	::WSACleanup();
+}
+
+// Overlapped 모델 (콜백 기반)
+/*
+struct Session
+{
 	// Overlapped Model (Base callback)
-	// 구조체 변수를 함수의 매개변수로 넘길 때, 
-	// 넘겨야 하는 타입을 구조체 속성에 첫번째로 두면 캐스팅할 때 구조체의 
-	// 첫번째 타입으로 변환할 수 있고, 함수에서 넘겨받은 구조체 속성으로 
+	// 구조체 변수를 함수의 매개변수로 넘길 때,
+	// 넘겨야 하는 타입을 구조체 속성에 첫번째로 두면 캐스팅할 때 구조체의
+	// 첫번째 타입으로 변환할 수 있고, 함수에서 넘겨받은 구조체 속성으로
 	// 구조체로 캐스팅하여 사용할 수 있음
 	WSAOVERLAPPED overlapped = {};
 
@@ -41,14 +208,6 @@ struct Session
 	// Overlapped Model
 	//WSAOVERLAPPED overlapped = {};
 };
-
-void CALLBACK RecvCallback(DWORD error, DWORD recvLen, LPWSAOVERLAPPED overlapped, DWORD flags)
-{
-	cout << "Data Recv Len Cal'lback = " << recvLen << endl;
-	// TODO : 에코 서버를 만든다면 WSASend()
-
-	Session* session = (Session*)overlapped;
-}
 
 int main()
 {
@@ -180,7 +339,7 @@ int main()
 	// 원속 종료
 	::WSACleanup();
 }
-
+*/
 // Overlapped Model (event base)
 /*
 int main()
