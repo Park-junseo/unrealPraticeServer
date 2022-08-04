55047d99cdc90046d56f13e5dea0e72c7bf668b1
박준서 <psbsanno.com@gmail.com>
2022-08-04 18:44:08 +0900

    Packet Serialization #2 ing

--- DummyClient/ClientPacketHandler.h
+++ DummyClient/ClientPacketHandler.h
@@ -14,3 +14,22 @@ public:
 	static void Handle_S_TEST(BYTE* buffer, int32 len);
 };
 
+template<typename T>
+class PacketList
+{
+public:
+	PacketList() :_data(nullptr), _count(0) {}
+	PacketList(T* data, uint16 count) : _data(data), _count(count) { }
+
+	T& operator[](uint16 index)
+	{
+		ASSERT_CRASH(index < _count);
+		return _data[index];
+	}
+
+	uint16 Count() { return _count; }
+
+private:
+	T*			_data;
+	uint16		_count;
+};
