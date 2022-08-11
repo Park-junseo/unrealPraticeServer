add0f8e8c45a5a3b3693144c1f28f7ae5418991b
박준서 <psbsanno.com@gmail.com>
2022-08-11 12:02:08 +0900

    JobQueue #1

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -8,7 +8,58 @@
 #include "BufferWriter.h"
 #include "ClientPacketHandler.h"
 #include "Protocol.pb.h"
+#include "Job.h"
+#include "Room.h"
 
+int main()
+{
+	// TEST JOB
+	{
+		// [일감 의뢰 내용] : 1번 유저한테 10만큼 힘을 줘라!
+		// 행동 : Heal
+		// 인자 : 1번 유저, 10이라는 힐량
+		HealJob healJob;
+		healJob._target = 1;
+		healJob._healValue = 10;
+
+		// 나-중에
+		healJob.Execute();
+	}
+
+	// JOB
+
+	ClientPacketHandler::Init();
+
+	ServerServiceRef service = MakeShared<ServerService>(
+		NetAddress(L"127.0.0.1", 7777),
+		MakeShared<IocpCore>(),
+		MakeShared<GameSession>, // TODO : SessionManager 등
+		100);
+
+	ASSERT_CRASH(service->Start());
+
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch([=]()
+			{
+				while (true)
+				{
+					service->GetIocpCore()->Dispatch();
+				}
+			});
+	}
+
+	while (true)
+	{
+		GRoom.FlushJob();
+		this_thread::sleep_for(1ms);
+	}
+
+	GThreadManager->Join();
+}
+
+// ... Chatting Practice
+/*
 int main()
 {
 	ClientPacketHandler::Init();
@@ -34,7 +85,7 @@ int main()
 
 	GThreadManager->Join();
 }
-
+*/
 // ...Packet Automatic
 /*
 int main()
