fd2a4902837d83ff7cb65796582d34050d59c567
박준서 <psbsanno.com@gmail.com>
2022-08-18 20:07:58 +0900

    DB Bind ing

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -39,6 +39,128 @@ void DoWorkerJob(ServerServiceRef& service)
 int main()
 {
 	//
+
+	// Thread 개수만큼 connectionCount를 부여할 수 있음
+	ASSERT_CRASH(GDBConnectionPool->Connect(1, L"Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\MSSQLLocalDB;Database=ServerDb;Trusted_Connection=Yes;"));
+
+	// Create Table
+	{
+		auto query = L"									\
+			DROP TABLE IF EXISTS [dbo].[Gold];			\
+			CREATE TABLE [dbo].[Gold]					\
+			(											\
+				[id] INT NOT NULL PRIMARY KEY IDENTITY, \
+				[gold] INT NULL, 						\
+				[name] NVARCHAR(50) NULL,				\
+				[createDate] DATETIME NULL				\
+			);";
+
+		DBConnection* dbConn = GDBConnectionPool->Pop();
+		ASSERT_CRASH(dbConn->Execute(query));
+		GDBConnectionPool->Push(dbConn);
+	}
+
+	// Add Data
+	for (int32 i = 0; i < 3; i++)
+	{
+
+		DBConnection* dbConn = GDBConnectionPool->Pop();
+		// 기존에 반인딩 된 정보 날림
+		dbConn->Unbind();
+
+		// 넘길 인자 바인딩
+		int32 gold = 100;
+		SQLLEN len = 0;
+
+		WCHAR name[100] = L"박준서";
+		SQLLEN nameLen = 0;
+
+		TIMESTAMP_STRUCT ts = {};
+		ts.year = 2021;
+		ts.month = 6;
+		ts.day = 5;
+		SQLLEN tsLen = 0;
+
+		// 넘길 인자 바인딩
+		ASSERT_CRASH(dbConn->BindParam(1, &gold, &len));
+		ASSERT_CRASH(dbConn->BindParam(2, name, &nameLen));
+		ASSERT_CRASH(dbConn->BindParam(3, &ts, &tsLen));
+
+		// SQL 실행
+		ASSERT_CRASH(dbConn->Execute(L"INSERT INTO [dbo].[Gold]([gold], [name], [createDate]) VALUES(?,?,?)"));
+		GDBConnectionPool->Push(dbConn);
+	}
+
+	// Read
+	{
+		DBConnection* dbConn = GDBConnectionPool->Pop();
+		// 기존에 바인딩 된 정보 날림
+		dbConn->Unbind();
+
+		int32 gold = 100;
+		SQLLEN len = 0;
+		// 넘길 인자 바인딩
+		ASSERT_CRASH(dbConn->BindParam(1, &gold, &len));
+
+		int32 outId = 0;
+		SQLLEN outIdLen = 0;
+		ASSERT_CRASH(dbConn->BindCol(2, &outId, &outIdLen));
+
+		int32 outGold = 0;
+		SQLLEN outGoldLen = 0;
+		ASSERT_CRASH(dbConn->BindCol(2, &outGold, &outGoldLen));
+
+		WCHAR outName[100];
+		SQLLEN outNameLen = 0;
+		ASSERT_CRASH(dbConn->BindCol(3, outName,len32(outName), &outNameLen));
+
+		TIMESTAMP_STRUCT outDate = {};
+		SQLLEN outDateLen = 0;
+		ASSERT_CRASH(dbConn->BindCol(4, &outDate, &outDateLen));
+
+		// SQL 실행
+		ASSERT_CRASH(dbConn->Execute(L"SELECT id, gold, name, createDate FROM [dbo].[Gold] WHERE gold = (?)"));
+
+		wcout.imbue(locale("kor"));
+
+		while (dbConn->Fetch())
+		{
+			wcout << "Id: " << outId << "Gold: " << outGold << " Name:" << outName << endl;
+			wcout << "Date: " << outDate.year << "/" << outDate.month << "/" << outDate.day << endl;
+		}
+		GDBConnectionPool->Push(dbConn);
+	}
+
+	//
+	ClientPacketHandler::Init();
+
+	ServerServiceRef service = MakeShared<ServerService>(
+		NetAddress(L"127.0.0.1", 7777),
+		MakeShared<IocpCore>(),
+		MakeShared<GameSession>, // TODO : SessionManager 등
+		100);
+
+	ASSERT_CRASH(service->Start());
+
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch([&service]()
+			{
+				DoWorkerJob(service);
+			});
+	}
+
+	// 메인 쓰레드도 균등하게 처리
+	DoWorkerJob(service);
+
+	GThreadManager->Join();
+}
+
+// DB Connection
+/*
+int main()
+{
+	//
 	
 	// Thread 개수만큼 connectionCount를 부여할 수 있음
 	ASSERT_CRASH(GDBConnectionPool->Connect(1, L"Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\MSSQLLocalDB;Database=ServerDb;Trusted_Connection=Yes;"));
@@ -131,7 +253,7 @@ int main()
 
 	GThreadManager->Join();
 }
-
+*/
 // JobTimer
 /*
 int main()
