fba571ee312d6538b90d146c0f3e0a2c2d198201
박준서 <psbsanno.com@gmail.com>
2022-08-22 16:01:16 +0900

    Procedure Generator

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -15,6 +15,7 @@
 #include "DBBind.h"
 #include "XmlParser.h"
 #include "DBSynchronizer.h"
+#include "GenProcedures.h"
 
 enum
 {
@@ -38,6 +39,75 @@ void DoWorkerJob(ServerServiceRef& service)
 		ThreadManager::DoGlobalQueueWork(); 
 	}
 }
+
+int main()
+{
+	// Thread 개수만큼 connectionCount를 부여할 수 있음
+	ASSERT_CRASH(GDBConnectionPool->Connect(1, L"Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\MSSQLLocalDB;Database=ServerDb;Trusted_Connection=Yes;"));
+
+	DBConnection* dbConn = GDBConnectionPool->Pop();
+	DBSynchronizer dbSync(*dbConn);
+	dbSync.Synchronize(L"GameDB.xml");
+	
+	{
+		WCHAR name[] = L"ParkJunseo";
+		SP::InsertGold insertGold(*dbConn);
+		insertGold.In_Gold(100);
+		insertGold.In_Name(name);
+		insertGold.In_CreateDate(TIMESTAMP_STRUCT{ 2022, 6, 8 });
+		insertGold.Execute();
+	}
+
+	{
+		SP::GetGold getGold(*dbConn);
+		getGold.In_Gold(100);
+
+		int32 id = 0;
+		int32 gold = 0;
+		WCHAR name[100];
+		TIMESTAMP_STRUCT date;
+
+		getGold.Out_Id(OUT id);
+		getGold.Out_Gold(OUT gold);
+		getGold.Out_Name(OUT name);
+		getGold.Out_CreateDate(OUT date);
+
+		getGold.Execute();
+
+		while (getGold.Fetch())
+		{
+			GConsoleLogger->WriteStdOut(Color::BLUE,
+				L"ID[%d] Gold[%d] Name[%s]\n", id, gold, name);
+		}
+	}
+
+	//
+	ClientPacketHandler::Init();
+
+	ServerServiceRef service = MakeShared<ServerService>(
+		NetAddress(L"127.0.0.1", 7777),
+		MakeShared<IocpCore>(),
+		MakeShared<GameSession>, // TODO : SessionManager 등
+		100);
+
+	ASSERT_CRASH(service->Start());
+
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch([&service]()
+			{
+				DoWorkerJob(service);
+			});
+	}
+
+	// 메인 쓰레드도 균등하게 처리
+	DoWorkerJob(service);
+
+	GThreadManager->Join();
+}
+
+// ORM
+/*
 
 int main()
 {
@@ -72,7 +142,7 @@ int main()
 
 	GThreadManager->Join();
 }
-
+*/
 // XML Parse
 /*
 int main()
